6d05f06d3df39841e446ba778da43871aa2f5d46
yzbao <yzbaoo@MacBook-Pro-5.local>
2019-10-18 19:13:55 +0800

    add: import react

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -10,7 +11,7 @@ module.exports = {
         // webpack所有文件的输出的路径，必须是绝对路径
         path: path.resolve('dist'),
         // 不会对生成文件的路径造成影响，主要是对你的页面里面引入的资源的路径做对应的补全，常见的就是css文件里面引入的图片
-        publicPath: '/'
+        // publicPath: '/'
     },
 
     /**
@@ -23,19 +24,19 @@ module.exports = {
             test: /\.(js|jsx)$/,
             loader: 'babel-loader',
             // include: 需要编译的目录
-            // include: /src/,
+            include: /src/,
             // exclude: 跳过的目录
             exclude: /node_modules/ 
         },{
             test: /\.css$/,
             // webpack1的写法 等同于 ['style-loader','css-loader']
-            use: 'style-loader!css-loader',
+            loader: 'style-loader!css-loader',
             include: /src/,
             exclude: /node_modules/ 
         },{
             test: /\.less$/,
             // 注意loader的加载顺序是从右向左，先用less-loader将less代码编译，再交给style-loader插入到网页里面去
-            use: 'style-loader!less-loader',
+            loader: 'style-loader!less-loader',
             include: /src/,
             exclude: /node_modules/ 
         },{
@@ -52,12 +53,22 @@ module.exports = {
             // include: /\.(woff|woff2|eot|ttf|otf)$/ 等等
             exclude: [/\.(js|mjs|jsx)$/, /\.html$/, /\.json$/],
             loader: 'file-loader'
-        }]
+        },{
+            test: /\.json$/,
+            exclude: /node_modules/,
+            loader: 'json-loader'
+        },]
     },
 
     devServer: {
         // 需要安装 webpack-dev-server npm install -D webpack-dev-server
         // 注意：安装完 webpack-dev-server，执行webpack-dev-server --open有可能会报错，此时有可能是webpack和webpack-dev-server版本不一致导致
-        contentBase: './dist'
-    }
+        // **坑** 在npm start时，修改的端口不生效，原因就是在start命令后没有指定 --config 的执行目录
+       port: 9999
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html' //使用一个模板
+        })
+    ]
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // webpack 入口文件
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>哈哈哈哈</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
