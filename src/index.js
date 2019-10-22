73cc5bad7dbbb419582b5831924d0e337e54b340
yzbao <yzbaoo@MacBook-Pro-5.local>
2019-10-22 18:01:11 +0800

    add: 添加可配置环境变量的文件

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    
     render() {
+        console.error(process.env);
         return (
             <div>
-                <h1>哈哈哈哈</h1>
+                <h1>环境: {process.env.NODE_ENV}</h1>
+                <h1>REACT_APP_API_SERVER: {process.env.REACT_APP_API_SERVER}</h1>
             </div>
         )
     }
