15868a4506a93a058e51ffc95490f583da347ecb
yzbao <yzbaoo@MacBook-Pro-5.local>
2019-10-23 15:31:38 +0800

    add: eslint "parser": "babel-eslint"

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
     
+    fn = (a,b) => {
+        console.error(a,b);
+    }
+
     render() {
+        const obj = {a:1,b:2,c:3,d:4};
+        obj.a =0;
+        console.error(obj);
         return (
             <div>
-                <h1>环境: {process.env.NODE_ENV}</h1>
+                  <h1>环境: {process.env.NODE_ENV}</h1>
                 <h1>REACT_APP_API_SERVER: {process.env.REACT_APP_API_SERVER}</h1>
             </div>
         )
